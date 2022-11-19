dafb1e58cd7ce930fde6c068747c08f1c9c5b828
Android <ajmalhmir@gmail.com>
2022-11-19 11:45:22 +0000

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_3/home_main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/home_main.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     // ignore: prefer_const_constructors
-//     return MaterialApp(
-//       home: HomePage(),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
