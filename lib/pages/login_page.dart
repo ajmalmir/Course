1345e00bc1c7384228921d185ff08fa6fb6ead0b
Android <ajmalhmir@gmail.com>
2022-11-19 15:11:21 +0000

    day 32

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,13 +4,8 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Material(
-        child: Center(
-      child: Text("Login",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.green,
-            fontWeight: FontWeight.bold,
-          )),
-    ));
+      child: 
+      
+    );
   }
 }
